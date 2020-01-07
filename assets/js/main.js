f9bd259b4ffdf813e78bb8f639e8b6e2395b3929
Lancer Abir <lancer_abir@blacks-MacBook-Air.local>
2020-01-07 19:02:33 +0600

    Esost-HTML-Project-04

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".click-one").click(function(){
+        $(".show-one").slideToggle(400);
+    });
+    $(".click-two").click(function(){
+        $(".show-two").slideToggle(400);
+    });
+    $(".click-three").click(function(){
+        $(".show-three").slideToggle(400);
+    });
+    $(".box-click-one").click(function(){
+        $(".box-show-one").slideToggle(400);
+    });
+    $(".box-click-two").click(function(){
+        $(".box-show-two").slideToggle(400);
+    });
+    $(".box-click-three").click(function(){
+        $(".box-show-three").slideToggle(400);
+    });
+    $(".box-click-four").click(function(){
+        $(".box-show-four").slideToggle(400);
+    });
+    $(".box-click-five").click(function(){
+        $(".box-show-five").slideToggle(400);
+    });
+   
+   
+});
+
